379cfdcd142e4da3e1971cda6ed3314e8de24e5b
Mohd Shahvez <119055434+mdshahvez24@users.noreply.github.com>
2024-09-07 00:51:56 +0530

    Update axiosInstance.js

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',  
+  baseURL: process.env.REACT_APP_API_URL || "https://ecomm-mernstack-backend.onrender.com",  
 });
 
 export default instance;
